c72aa83be8d45371047c0c21aa48090f36230045
Lucas Alcântara <lucashmalcantara@gmail.com>
2019-01-06 16:57:26 -0200

    Adicionado suporte a Database Migrations com a biblioteca Evolve.

---
+++ RestfulAspNetCore - 06 Adicionando suporte a Database Migrations/RestfulAspNetCore/Services/Implementacoes/PessoaService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using RestfulAspNetCore.Model;
+using RestfulAspNetCore.Model.Context;
+using RestfulAspNetCore.Repositories;
+
+namespace RestfulAspNetCore.Services.Implementacoes
+{
+    // Responsável pelas regras de negócio e validações.
+    public class PessoaService : IPessoaService
+    {
+        private IPessoaRepository _repository;
+
+        public PessoaService(IPessoaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Pessoa Criar(Pessoa pessoa)
+        {
+            return _repository.Criar(pessoa);
+        }
+
+        public Pessoa Atualizar(Pessoa pessoa)
+        {
+            return _repository.Atualizar(pessoa);
+        }
+
+        public void Excluir(long id)
+        {
+            _repository.Excluir(id);
+        }
+
+        public Pessoa RetornarPorId(long id)
+        {
+            return _repository.RetornarPorId(id);
+        }
+
+        public List<Pessoa> RetornarTodos()
+        {
+            return _repository.RetornarTodos();
+        }
+    }
+}
