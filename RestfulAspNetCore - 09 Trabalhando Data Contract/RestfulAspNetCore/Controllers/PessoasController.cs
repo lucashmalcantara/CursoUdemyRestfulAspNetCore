5b6a657d4257d72f1eda099f57804855f96741a3
Lucas Alcântara <lucashmalcantara@gmail.com>
2019-01-11 18:01:26 -0200

    Trabalhando Data Contract.

---
+++ RestfulAspNetCore - 09 Trabalhando Data Contract/RestfulAspNetCore/Controllers/PessoasController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestfulAspNetCore.Data.ValueObjects;
+using RestfulAspNetCore.Services;
+
+namespace RestfulAspNetCore.Controllers
+{
+    // Utilizando a arquitetura adotada neste projeto:
+    // Controller: vai rotear a requisição para o método correto.
+    // Services: vai validar e conter as regras de negócios.
+    // Repositories: acesso aos dados e persistência.
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class PessoasController : Controller
+    {
+        private readonly IPessoaService _pessoaService;
+
+        public PessoasController(IPessoaService pessoaService)
+        {
+            _pessoaService = pessoaService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_pessoaService.RetornarTodos());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var pessoa = _pessoaService.RetornarPorId(id);
+
+            if (pessoa == null) return NotFound();
+
+            return Ok(pessoa);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]PessoaVo pessoa)
+        {
+            if (pessoa == null) return BadRequest();
+
+            return new ObjectResult(
+                _pessoaService.Criar(pessoa));
+        }
+
+        // PUT api/values/
+        [HttpPut]
+        public IActionResult Put([FromBody]PessoaVo pessoa)
+        {
+            if (pessoa == null) return BadRequest();
+
+            var pessoaAtualizada = _pessoaService.Atualizar(pessoa);
+
+            if (pessoaAtualizada == null) return NoContent();
+
+            return new ObjectResult(pessoaAtualizada);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _pessoaService.Excluir(id);
+            return NoContent();
+        }
+    }
+}
---
+++ RestfulAspNetCore - 09 Trabalhando Data Contract/RestfulAspNetCore/Repositories/Generics/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using RestfulAspNetCore.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulAspNetCore.Repositories.Generics
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Criar(T item);
+        T RetornarPorId(long id);
+        List<T> RetornarTodos();
+        T Atualizar(T item);
+        void Excluir(long id);
+        bool Existe(long? id);
+    }
+}
