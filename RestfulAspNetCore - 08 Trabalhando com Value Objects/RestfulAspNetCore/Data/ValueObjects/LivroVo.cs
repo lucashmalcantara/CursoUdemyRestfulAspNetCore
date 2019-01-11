5a3bfccbb63f09981ddd83fc2743908447b72fc4
Lucas Alcântara <lucashmalcantara@gmail.com>
2019-01-11 17:16:10 -0200

    Trabalhando com Value Objects.

---
+++ RestfulAspNetCore - 08 Trabalhando com Value Objects/RestfulAspNetCore/Data/ValueObjects/LivroVo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulAspNetCore.Data.ValueObjects
+{
+    public class LivroVo
+    {
+        public long? Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataLancamento { get; set; }
+    }
+}
---
+++ RestfulAspNetCore - 08 Trabalhando com Value Objects/RestfulAspNetCore/Services/Implementacoes/LivroService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using RestfulAspNetCore.Data.Converters;
+using RestfulAspNetCore.Data.ValueObjects;
+using RestfulAspNetCore.Model;
+using RestfulAspNetCore.Model.Context;
+using RestfulAspNetCore.Repositories;
+using RestfulAspNetCore.Repositories.Generics;
+
+namespace RestfulAspNetCore.Services.Implementacoes
+{
+    // Responsável pelas regras de negócio e validações.
+    public class LivroService : ILivroService
+    {
+        private readonly IRepository<Livro> _repository;
+        private readonly LivroConverter _converter;
+
+        public LivroService(IRepository<Livro> repository)
+        {
+            _repository = repository;
+            _converter = new LivroConverter();
+        }
+
+        public LivroVo Criar(LivroVo pessoa)
+        {
+            var livroModel = _converter.Trasformar(pessoa);
+            livroModel = _repository.Criar(livroModel);
+            return _converter.Trasformar(livroModel);
+        }
+
+        public LivroVo Atualizar(LivroVo pessoa)
+        {
+            var livroModel = _converter.Trasformar(pessoa);
+            livroModel = _repository.Atualizar(livroModel);
+            return _converter.Trasformar(livroModel);
+        }
+
+        public void Excluir(long id)
+        {
+            _repository.Excluir(id);
+        }
+
+        public LivroVo RetornarPorId(long id)
+        {
+            var livroModel = _repository.RetornarPorId(id);
+            return _converter.Trasformar(livroModel);
+        }
+
+        public List<LivroVo> RetornarTodos()
+        {
+            var listaLivroModel = _repository.RetornarTodos();
+            return _converter.Transformar(listaLivroModel);
+        }
+    }
+}
