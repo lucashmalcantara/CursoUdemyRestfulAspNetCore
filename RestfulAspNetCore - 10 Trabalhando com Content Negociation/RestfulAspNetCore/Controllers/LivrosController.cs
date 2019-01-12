fe67f0b5268060242c1231c9f13f2bddc9f26d36
Lucas Alcântara <lucashmalcantara@gmail.com>
2019-01-12 18:29:50 -0200

    Trabalhando com Content Negociation.

---
+++ RestfulAspNetCore - 10 Trabalhando com Content Negociation/RestfulAspNetCore/Controllers/LivrosController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestfulAspNetCore.Data.ValueObjects;
+using RestfulAspNetCore.Services;
+
+namespace RestfulAspNetCore.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class LivrosController : Controller
+    {
+        private readonly ILivroService _livroService;
+
+        public LivrosController(ILivroService livroService)
+        {
+            _livroService = livroService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_livroService.RetornarTodos());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var livro = _livroService.RetornarPorId(id);
+
+            if (livro == null) return NotFound();
+
+            return Ok(livro);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]LivroVo livro)
+        {
+            if (livro == null) return BadRequest();
+
+            return new ObjectResult(
+                _livroService.Criar(livro));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody]LivroVo livro)
+        {
+            if (livro == null) return BadRequest();
+
+            var livroAtualizado = _livroService.Atualizar(livro);
+
+            if (livroAtualizado == null) return NoContent();
+
+            return new ObjectResult(livroAtualizado);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _livroService.Excluir(id);
+            return NoContent();
+        }
+    }
+}
---
+++ RestfulAspNetCore - 10 Trabalhando com Content Negociation/RestfulAspNetCore/Data/ValueObjects/PessoaVo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulAspNetCore.Data.ValueObjects
+{
+    // O padrão VO auxilia na segurança da aplicação.Evita expor a classe que representa nosso banco.Cria-se uma nova classe responsável para a exposição dos dados.
+    public class PessoaVo
+    {
+        public long? Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Endereco { get; set; }
+        public char Genero { get; set; }
+
+    }
+}
